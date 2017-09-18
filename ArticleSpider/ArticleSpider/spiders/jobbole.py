33eb224470f14f203c1dea1e5c311825eb2cc6c0
aqie123 <2924811900@qq.com>
2017-09-18 18:03:58 +0800

    爬取伯乐在线

--- ArticleSpider/ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/ArticleSpider/spiders/jobbole.py
@@ -5,7 +5,13 @@ import scrapy
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
     allowed_domains = ['blog.jobbole.com']
-    start_urls = ['http://blog.jobbole.com/']
+    start_urls = ['http://blog.jobbole.com/112381/']
 
     def parse(self, response):
+        # 下标从第三个开始
+        # /html/body/div[3]/div[3]/div[1]/div[1]/h1  js 生成的不算
+        # // *[ @ id = "post-112381"] / div[1] / h1
+        title = response.xpath('//*[ @ id = "post-112381"]/div[1]/h1/text()')
+        create_date = response.xpath('//*[@id="post-112381"]/div[2]/p/text()')
+        create_date = date.extract()[0].strip().replace('·','').strip()
         pass
--- chapter3/note.py
+++ chapter3/note.py
@@ -20,6 +20,14 @@
             继续配置 main.py
             settings.py 设置robots  False
             在 jobbole.py 断点调试
+        d.  对当前页面做调试
+            scrapy shell http://blog.jobbole.com/112381/
+                1. title = response.xpath('//*[ @ id = "post-112381"]/div[1]/h1/text()')
+                   获取title 值 title.extract()
+                2. 文章日期; date = response.xpath('//*[@id="post-112381"]/div[2]/p/text()')
+                    date.extract()[0].strip().replace('·','').strip()
+                3.点赞 digg = response.xpath('//*[@id="post-112381"]/div[3]/div[8]/span[1]/h10/text()')
+                       digg.extract()[0]
     3.xpath
         a.使用路径表达式在xml和html中进行导航
         b.包含标准函数库
@@ -37,5 +45,20 @@
             4.//div 选取所有div子元素
             5.article//div 选取属于article元素的后代div
             6.选取所有名为class的属性
+            谓语：
+            7. /article/div[1]  第一个div
+            8. /article/div[last()]  选取最后一个div元素
+            9. /article/div[last()-1] 倒数第二个
+            10. //div[@lang]  选取所有拥有lang属性的div元素
+            11. //div[@lang='eng']  选取所有lang属性为eng的div元素
+            其他
+            12./div/*  选取div元素所有子节点
+            13.//*     选取所有元素
+            14.//div[@*]  选取所有带属性的title元素
+            15./div/a | //div/p  选取所有div元素的a和p元素
+            16.//span |//ul  选取文档中的span和ul元素
+            17.article/div/p|//span  选取所有属于article元素的div元素的p元素
+                                    以及文档中所有的span元素
+
 
 """
